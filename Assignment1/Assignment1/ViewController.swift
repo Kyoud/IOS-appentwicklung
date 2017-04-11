f62e4645c4b3c4752a6608d2d179fe82f326528e
app <app@imac06.fbi.h-da.de.local>
2017-04-11 16:38:29 +0200

    tolle faben

--- Assignment1/Assignment1/ViewController.swift
+++ Assignment1/Assignment1/ViewController.swift
@@ -10,14 +10,13 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-    }
+    @IBOutlet weak var Display: UITextField!
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    @IBAction func touchDigit(_ sender: UIButton) {
+        print("Hello World!")
+        print(sender.currentTitle!)
+        Display.text = Display.text! + sender.currentTitle!
+        Display.backgroundColor = sender.backgroundColor
     }
 
 
